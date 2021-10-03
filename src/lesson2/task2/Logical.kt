f743612f00c2317ae972d8f269706ba4650a8b33
uphoros <71212641+uphoros@users.noreply.github.com>
2021-10-03 22:14:57 +0300

    опять переделал brickPasses
    (cherry picked from commit ae657f2ab99c9ffa2daa3eddf52c5b2c35816c89)

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -61,14 +61,14 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = //((b<=r)&&(c<=s))||((a<=r)&&(b<=s))
-    (((maxOf(a,b)>=maxOf(r,s))&&(minOf(a,b)>=minOf(r,s)))||((maxOf(c,b)>=maxOf(r,s))&&(minOf(c,b)>=minOf(r,s))))
-/**{
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean //= ((b<=r)&&(c<=s))||((a<=r)&&(b<=s))
+    //(((maxOf(a,b)>=maxOf(r,s))&&(minOf(a,b)>=minOf(r,s)))||((maxOf(c,b)>=maxOf(r,s))&&(minOf(c,b)>=minOf(r,s))))
+{
     val l = mutableListOf(a, b, c)
-    if (maxOf(a, b, c) < maxOf(r, s)) return false
+    //if (maxOf(a, b, c) < maxOf(r, s)) return false
     l.remove(maxOf(a, b, c))
-    return (maxOf(l[0],l[1]) >= min(r, s))
+    return (maxOf(l[0],l[1]) <= min(r, s))
 }
-*/
+
 
 
