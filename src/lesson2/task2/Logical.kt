16855bd107871373536ad7923aedaffc1fb9f493
uphoros <71212641+uphoros@users.noreply.github.com>
2021-10-03 22:07:25 +0300

    переделал brickPasses
    (cherry picked from commit e611055e4510166ff78dba57b656a01553f4b3b2)

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -61,7 +61,7 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = ((b>=r)&&(c>=s))||((a>=r)&&(b>=s))
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = ((b<=r)&&(c<=s))||((a<=r)&&(b<=s))
     //(((maxOf(a,b)>=maxOf(r,s))&&(minOf(a,b)>=minOf(r,s)))||((maxOf(c,b)>=maxOf(r,s))&&(minOf(c,b)>=minOf(r,s))))
 /**{
     val l = mutableListOf(a, b, c)
