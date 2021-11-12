0d909466a6741c1697c8a8dd8d4142e5bfb223ee
GoshaSilaev <90410842+GoshaSilaev@users.noreply.github.com>
2021-11-12 15:43:03 +0300

    Update List.kt

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -262,10 +262,7 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  * 90 = XC, 100 = C, 400 = CD, 500 = D, 900 = CM, 1000 = M.
  * Например: 23 = XXIII, 44 = XLIV, 100 = C
  */
-fun roman(n: Int): String {
-    
-}
-
+fun roman(n: Int): String = TODO()
 /**
  * Очень сложная (7 баллов)
  *
